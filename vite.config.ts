812987737130bc45332d3851c470fd8974673733
treewind <jeong7@gmail.com>
2025-11-30 19:25:48 +0900

    web

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    // Netlify or local env variables need to be exposed to the client
-    'process.env': process.env
-  }
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, (process as any).cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      // Safely expose specific env vars to the client
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    }
+  };
 });
